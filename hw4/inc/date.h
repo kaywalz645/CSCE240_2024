1daa114f6ea3c6eb0a6c762aa33bb033a5c0cfda
kaywalz645 <krbair0928@gmail.com>
2024-06-11 22:49:13 -0400

    update

--- hw4/inc/date.h
+++ hw4/inc/date.h
@@ -1,20 +1,6 @@
-//  This class tests your ability to manage operator overloading and iterating
-//  over arrays via pointer (pointer arithmetic).
-//
-//  Points:
-//   Style : 1
-//   Compilation : 0.5
-//    + Date::Date(std::string&) : 1
-//    + Date::Date(std::string*, std::string*) : 1
-//    + Date::operator-(Date&) 
-//      && Date::operator-(std::string&) : 1, using Date::Date(std::string&)
-//    + operator-(std::string&, Date&) : 1
-//
-//   Bonus:
-//     Implicit conversion to std::string: 1, using Date::Date(std::string&)
-//
-#ifndef HW4_INC_DATE_H
-#define HW4_INC_DATE_H
+// Copyright 2024 Kaylee Walz
+#ifndef HW4_INC_DATE_H_
+#define HW4_INC_DATE_H_
 
 #include <istream>
 #include <ostream>
@@ -23,67 +9,34 @@
 
 class Date {
  public:
+     Date();
 
-  Date();
-
-  Date(const std::string& month,
+     Date(const std::string& month,
        const std::string& day,
        const std::string& year);
 
 
-  // Accepts as parameters a string array's bounds, exclusive of the end
-  // pointer,
-  //   I.E., The two pointer represent [begin, end),
-  //   I.E., The end pointer does not point to an element of the array,
-  //   E.G.,
-  //     begin ->  "mm",
-  //               "dd",
-  //               [ "yy" | "yyyy" ],
-  //     end ->
-  //   I.E., The end pointer stores the address of the first byte after the
-  //   last element of the array. This is idiomatic of container bounds in the
-  //   STL.
-  //
-  Date(const std::string* begin, const std::string* end);
+     Date(const std::string* begin, const std::string* end);
 
 
-  // The parameter date is of the form
-  //   mm/dd/yy or mm/dd/yyyy
-  //            or
-  //   mm-dd-yy or mm-dd-yyyy
-  explicit Date(const std::string& date);
+     explicit Date(const std::string& date);
+
+     const std::string& month() const;
+     const std::string& day() const;
+     const std::string& year() const;
 
 
-  // Not explicitly tested; used to test Date constructors
-  const std::string& month() const;
-  const std::string& day() const;
-  const std::string& year() const;
+     int operator-(const Date& end_date);
+     int operator-(const std::string& end_date);
 
-
-  // Tested in a single test; both must pass for credit.
-  // Calculates the number of whole years between the calling class and the
-  // parameters class: years = begin_date - end_date; may be negative.
-  //
-  int operator-(const Date& end_date);
-  // The parameter end_date is of the form
-  //   mm/dd/yy or mm/dd/yyyy
-  //            or
-  //   mm-dd-yy or mm-dd-yyyy
-  int operator-(const std::string& end_date);
-
-  private:
-    std::string month_;
-    std::string day_;
-    std::string year_;
+ private:
+     std::string month_;
+     std::string day_;
+     std::string year_;
 
     void parseDate(const std::string& date);
-
 };
 
-
-// The parameter begin_date is of the form
-//   mm/dd/yy or mm/dd/yyyy
-//            or
-//   mm-dd-yy or mm-dd-yyyy
 int operator-(const std::string& begin_date, const Date& end_date);
-#endif //Date
+
+#endif  // HW4_INC_DATE_H_
--- hw4/src/date.cc
+++ hw4/src/date.cc
@@ -1,21 +1,5 @@
-//Copyright 2024 Kaylee Walz
+// Copyright 2024 Kaylee Walz
 #include <hw4/inc/date.h>
-
-//  This class tests your ability to manage operator overloading and iterating
-//  over arrays via pointer (pointer arithmetic).
-//
-//  Points:
-//   Style : 1
-//   Compilation : 0.5
-//    + Date::Date(std::string&) : 1
-//    + Date::Date(std::string*, std::string*) : 1
-//    + Date::operator-(Date&) 
-//      && Date::operator-(std::string&) : 1, using Date::Date(std::string&)
-//    + operator-(std::string&, Date&) : 1
-//
-//   Bonus:
-//     Implicit conversion to std::string: 1, using Date::Date(std::string&)
-//
 
 #include <cassert>
 #include <istream>
@@ -26,35 +10,16 @@
 
 
 
-  // Not explicitly tested, but you may want this for yourself; why is that?
-  //
-  Date::Date(){
-    //empty
+  Date::Date() {
+    // empty
   }
 
-
-  // Not explicity tested; implement for yourself if you need for Demographics
-  //
   Date::Date(const std::string& month,
        const std::string& day,
        const std::string& year): month_(month), day_(day), year_(year)  {
     // empty
   }
 
-
-  // Accepts as parameters a string array's bounds, exclusive of the end
-  // pointer,
-  //   I.E., The two pointer represent [begin, end),
-  //   I.E., The end pointer does not point to an element of the array,
-  //   E.G.,
-  //     begin ->  "mm",
-  //               "dd",
-  //               [ "yy" | "yyyy" ],
-  //     end ->
-  //   I.E., The end pointer stores the address of the first byte after the
-  //   last element of the array. This is idiomatic of container bounds in the
-  //   STL.
-  //
   Date::Date(const std::string* begin, const std::string* end) {
     if (end - begin == 3) {
         month_ = *(begin);
@@ -65,11 +30,6 @@
     }
   }
 
-
-  // The parameter date is of the form
-  //   mm/dd/yy or mm/dd/yyyy
-  //            or
-  //   mm-dd-yy or mm-dd-yyyy
     Date::Date(const std::string& date) {
         parseDate(date);
     }
@@ -82,7 +42,6 @@
     std::getline(iss, year_, delimiter);
     }
 
-  // Not explicitly tested; used to test Date constructors
   const std::string& Date::month() const {
     return month_;
   }
@@ -93,28 +52,15 @@
     return year_;
   }
 
-
-  // Tested in a single test; both must pass for credit.
-  // Calculates the number of whole years between the calling class and the
-  // parameters class: years = begin_date - end_date; may be negative.
-  //
   int Date::operator-(const Date& end_date) {
     return std::stoi(year_) - std::stoi(end_date.year_);
   }
-  // The parameter end_date is of the form
-  //   mm/dd/yy or mm/dd/yyyy
-  //            or
-  //   mm-dd-yy or mm-dd-yyyy
+
   int Date::operator-(const std::string& end_date) {
      Date temp(end_date);
     return *this - temp;
   }
 
-
-// The parameter begin_date is of the form
-//   mm/dd/yy or mm/dd/yyyy
-//            or
-//   mm-dd-yy or mm-dd-yyyy
 int operator-(const std::string& begin_date, const Date& end_date) {
     Date temp(begin_date);
     return temp - end_date;
